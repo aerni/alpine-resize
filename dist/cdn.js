bf5af243346bc1292d38ae9dfeff9301b1373d93
Rasso Hilber <mail@rassohilber.com>
2022-11-23 10:37:10 +0100

    Build dist files

--- dist/cdn.js
+++ dist/cdn.js
@@ -4,7 +4,8 @@
     Alpine.directive("resize", (el, {expression}, {evaluateLater, cleanup}) => {
       const evaluate = evaluateLater(expression);
       const observer = new ResizeObserver((entries) => {
-        entries.forEach((entry) => evaluate());
+        entries.forEach((entry) => evaluate(() => {
+        }, {params: [entry]}));
       });
       observer.observe(el);
       cleanup(() => {
