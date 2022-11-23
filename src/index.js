bd0662d569662606093ed2cca88755204cd624b7
Rasso Hilber <mail@rassohilber.com>
2022-11-23 10:35:33 +0100

    Add a reference to the `ResizeObserverEntry` as parameter to `evaluate()`

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 export default function (Alpine) {
     Alpine.directive(
         'resize', (el, { expression }, { evaluateLater, cleanup }) => {
+            console.log(expression);
             const evaluate = evaluateLater(expression)
 
             const observer = new ResizeObserver(entries => {
-                entries.forEach(entry => evaluate())
+                entries.forEach(entry => evaluate(() => {}, { params: [entry] }))
             })
 
             observer.observe(el)
